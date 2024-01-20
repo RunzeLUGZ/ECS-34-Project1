50e6bba2dc7747602743be6a7ca785e35cf72ad7
RunzeLUGZ <ericlugz@outlook.com>
2024-01-19 22:44:33 -0800

    1.19

--- testsrc/StringUtilsTest.cpp
+++ testsrc/StringUtilsTest.cpp
@@ -1,14 +1,16 @@
 #include <gtest/gtest.h>
 #include "StringUtils.h"
 
-std::string Base = "Hello";
-
 TEST(StringUtilsTest, SliceTest){
+    std::string Base = "Hello";
+
     EXPECT_EQ(StringUtils::Slice(Base,0), "Hello");
     EXPECT_EQ(StringUtils::Slice(Base,1), "ello");
 }
 
 TEST(StringUtilsTest, Capitalize){
+    std::string Base = "hello";
+
     EXPECT_EQ(StringUtils::Capitalize(Base), "Hello");
     EXPECT_EQ(StringUtils::Capitalize(std::string()), "");
     EXPECT_EQ(StringUtils::Capitalize(" hello"), " hello");
